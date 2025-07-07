2b3c11674d1522663ed54dd2243098f97620f957
lloyd <lloydgutierrez24@gmail.com>
2025-07-07 12:37:00 +0800

    client - updated main-layout

--- client/src/app/layouts/main/main-layout.component.spec.ts
+++ client/src/app/layouts/main/main-layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainLayoutComponent } from './main-layout.component';
+import { provideRouter } from '@angular/router';
 
 describe('MainLayoutComponent', () => {
   let component: MainLayoutComponent;
@@ -8,9 +9,10 @@ describe('MainLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainLayoutComponent]
+      imports: [MainLayoutComponent],
+      providers: [provideRouter([])]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(MainLayoutComponent);
     component = fixture.componentInstance;
